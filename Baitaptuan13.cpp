6b30e41c69b977136600805e6878f44187b1bd8e
TranMinhDucbk <tranduciq@gmail.com>
2026-06-03 21:28:30 +0700

    Update Baitaptuan13.cpp

--- Baitaptuan13.cpp
+++ Baitaptuan13.cpp
@@ -1,17 +1,27 @@
 #include<iostream>
 using namespace std;
-
-struct Node
-{
+// khai báo nút
+struct Node {
     int data;
     Node *left;
     Node *right;
 };
-Node *taonode(int a)
-{
+// tạo nút
+Node *taonode(int a){   
     Node *p = new Node;
     p->data = a;
     p->left = NULL;
     p->right = NULL;
     return p;
+}
+// thêm nút vô cây
+void themnode(Node *&nodethem, int a) {
+    if (nodethem == NULL)
+    {nodethem = taonode(a);}
+    else{
+        if (a < nodethem->data){     // nhỏ hơn sang trái
+            themnode(nodethem->left, a); }
+        else{
+            themnode(nodethem->right, a); } //còn lại sang phải
+    }
 }
