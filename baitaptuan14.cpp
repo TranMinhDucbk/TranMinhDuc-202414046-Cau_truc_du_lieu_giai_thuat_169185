5f35c2fde7171731c4208bcf5f7d46ab46ba4a86
TranMinhDucbk <tranduciq@gmail.com>
2026-06-10 21:32:38 +0700

    Update baitaptuan14.cpp

--- baitaptuan14.cpp
+++ baitaptuan14.cpp
@@ -18,11 +18,30 @@ int max(int a, int b) {
     return b;
  }
 }
-Node* createNode(int x) {
+Node* taoNode(int x) {
     Node* p = new Node;
     p->data = x;
     p->left = NULL;
     p->right = NULL;
     p->chieucao = 1;
     return p;
+}
+Node* XoayPhai(Node* p) {   //xoay sang phải
+    Node* x = p->left;
+    Node* T = x->right;
+x->right = p;
+    p->left = T;
+    p->chieucao = max(chieucao(p->left), chieucao(p->right)) + 1;
+    x->chieucao = max(chieucao(x->left), chieucao(x->right)) + 1;
+    return x;
+}
+Node* XoayTrai(Node* q) {  //xoay sang trái
+    Node* p = q->right;
+    Node* T = p->left;
+p->left = x;
+    q->right = T;
+q->chieucao = max(chieucao(q->left), chieucao(q->right)) + 1;
+    p->chieucao = max(chieucao(p->left), chieucao(p->right)) + 1;
+
+    return y;
 }
