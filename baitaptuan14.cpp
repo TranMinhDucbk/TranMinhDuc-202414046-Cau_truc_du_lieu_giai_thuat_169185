cfd6accc599904e85b9c5008942a49f329b066b4
TranMinhDucbk <tranduciq@gmail.com>
2026-06-10 21:25:58 +0700

    Update baitaptuan14.cpp

--- baitaptuan14.cpp
+++ baitaptuan14.cpp
@@ -9,5 +9,20 @@ struct Node {
 };
  int chieucao(Node* c){
    if ( p = NULL){ return 0;}
-     return c->chieucao;
+     return c->chieucao ;
   }
+int max(int a, int b) {
+    if (a > b){
+        return a;}
+    else{
+    return b;
+ }
+}
+Node* createNode(int x) {
+    Node* p = new Node;
+    p->data = x;
+    p->left = NULL;
+    p->right = NULL;
+    p->chieucao = 1;
+    return p;
+}
