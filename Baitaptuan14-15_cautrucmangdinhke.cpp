6f9349db072f5af3a25769f38f05ef6707185dbf
TranMinhDucbk <tranduciq@gmail.com>
2026-06-17 21:46:34 +0700

    Update Baitaptuan14-15_cautrucmangdinhke.cpp

--- Baitaptuan14-15_cautrucmangdinhke.cpp
+++ Baitaptuan14-15_cautrucmangdinhke.cpp
@@ -8,3 +8,21 @@ void themcanh(int u, int v) { // kết nối các tỉnh
     a[u][v] = 1;
     a[v][u] = 1; }
 
+void duyettheochieurong(int bandau) { // duyệt theo chiều rộng
+    bool daduyet[11] = {false};
+queue<int> q;
+daduyet[bandau] = true;
+    q.push(bandau);
+    int stt = 1;
+    while (!q.empty())  {
+        int u = q.front();
+        q.pop();
+cout << stt++ << ". " << ten[u] << endl;
+for (int v = 0; v < 11; v++){
+            if (a[u][v] == 1 && daduyet[v] == false){
+                daduyet[v] = true;
+                q.push(v)
+            }
+        }
+    }
+}
