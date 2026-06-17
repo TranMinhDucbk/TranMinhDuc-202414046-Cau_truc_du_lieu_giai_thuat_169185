3e437e45006117ab2bceddffea7528a0d36ab633
TranMinhDucbk <tranduciq@gmail.com>
2026-06-17 21:39:22 +0700

    Update Baitaptuan14-15_ctmocnoi.cpp

--- Baitaptuan14-15_ctmocnoi.cpp
+++ Baitaptuan14-15_ctmocnoi.cpp
@@ -9,3 +9,21 @@ struct Node
 };
 Node *ds[11];
 string tinh[11] = {"HN","TN","BN","BG","UB","HP","HD","HY","PL", "HB","ST"};
+
+Node* taoNode(int node) 
+    Node *p = new Node;
+    p->data = node;
+    p->next = NULL;
+return p; }
+void themvaocuoi(Node *&dau, int x){
+    Node *p = taoNode(x);
+
+    if(dau == NULL){
+        dau = p;
+        return 0;}
+Node *q = dau;
+while(q->next != NULL){
+        q = q->next;
+    }
+q->next = p;
+}
