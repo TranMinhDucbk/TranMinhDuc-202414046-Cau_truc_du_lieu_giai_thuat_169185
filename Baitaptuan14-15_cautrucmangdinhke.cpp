a51d34ea0e96cf7281285b53ba7a2c1413cacb13
TranMinhDucbk <tranduciq@gmail.com>
2026-06-17 21:35:07 +0700

    Update Baitaptuan14-15_cautrucmangdinhke.cpp

--- Baitaptuan14-15_cautrucmangdinhke.cpp
+++ Baitaptuan14-15_cautrucmangdinhke.cpp
@@ -4,3 +4,7 @@ using namespace std;
 
 string tinh[11] = {"HN","TN","BN","BG","UB","HP","HD","HY","PL", "HB","ST"};
 
+void themcanh(int u, int v) { // kết nối các tỉnh
+    a[u][v] = 1;
+    a[v][u] = 1; }
+
