dd0bd9e2fec6ea478dcdda639aad68d18cbc4e15
TranMinhDucbk <tranduciq@gmail.com>
2026-06-17 21:59:03 +0700

    Update Baitaptuan14-15_cautrucmangdinhke.cpp

--- Baitaptuan14-15_cautrucmangdinhke.cpp
+++ Baitaptuan14-15_cautrucmangdinhke.cpp
@@ -17,7 +17,7 @@ daduyet[bandau] = true;
     while (!q.empty())  {
         int u = q.front();
         q.pop();
-cout << stt++ << ". " << ten[u] << endl;
+cout << stt++ << " " << ten[u] << endl;
 for (int v = 0; v < 11; v++){
             if (a[u][v] == 1 && daduyet[v] == false){
                 daduyet[v] = true;
@@ -25,4 +25,29 @@ for (int v = 0; v < 11; v++){
             }
         }
     }
+}
+int main()
+ {
+    themvaocanh(0,1);   // HN - TN
+    themvaocanh(0,2);   // HN - BN
+    themvaocanh(0,6);   // HN- HD
+    themvaocanh(0,8);   // HN- PL
+    themvaocanh(0,9);   // HN - HB
+     themvaocanh(0,10);  // HN - ST
+     
+    themvaocanh(2,3);   // BN- BG
+    themvaocanh(2,4);   // BN- UB
+   
+    themvaocanh(3,4);   // BG - UB
+   
+    themvaocanh(4,5);   // UB-HP
+   
+    themvaocanh(5,6);   // HP - HD
+   
+    themvaocanh(6,7);   // HD- HY
+   
+    themvaocanh(7,8);   // HY- PL
+    cout << "BFS bắt đầu từ HN";
+    duyettheochieurong(0);
+return 0;
 }
