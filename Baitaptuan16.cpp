ed81ae650a85084ea5dcefa057a47249f9759986
TranMinhDucbk <tranduciq@gmail.com>
2026-06-24 19:49:27 +0700

    Update Baitaptuan16.cpp

--- Baitaptuan16.cpp
+++ Baitaptuan16.cpp
@@ -31,3 +31,13 @@ void them_canh_noi(DoThi *do_thi, int u, int v) {
     do_thi->ma_tran_ke[u][v] = 1;
     do_thi->ma_tran_ke[v][u] = 1;
 }
+void tim_duong_ngan_nhat(DoThi *do_thi, int diem_xuat_phat, int diem_dich) {
+    int khoang_cach[SO_THANH_PHO];
+    bool da_duyet[SO_THANH_PHO];
+    int vet_duong_di[SO_THANH_PHO];
+ for (int i = 0; i < do_thi->so_luong_dinh; i++) 
+        khoang_cach[i] = VO_CUC;
+        da_duyet[i] = false;
+        vet_duong_di[i] = -1
+    }
+   khoang_cach[diem_xuat_phat] = 0;
