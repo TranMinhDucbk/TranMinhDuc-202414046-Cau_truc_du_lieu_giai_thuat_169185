444ea6983631816a355ae1e4d4c6c178bf983a54
TranMinhDucbk <tranduciq@gmail.com>
2026-06-29 23:14:13 +0700

    Update baitaptuan18.cpp

--- baitaptuan18.cpp
+++ baitaptuan18.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 const int Sotinh_max = 11;
 const int INF = 1e9;    
-
+// tạo ma trận kề như slide
 const string DSTinh[Sotinh_max] = { "HN", "TN", "BN", "BG", "UB", "HP", "HD", "HY", "PL", "HB", "ST" }; 
 
 struct Matran {   // khai báo
@@ -14,7 +14,7 @@ struct Matran {   // khai báo
     int matrix[Sotinh_max][Sotinh_max];
 };
 
-void Khoitaomatran(Matran &g, int n) { // tạo mtr trống (0)
+void Khoitaomatran(Matran &g, int n) { 
     g.Sotinhxet = n;
     for (int i = 0; i < n; i++) {     // mtr trống
         for (int j = 0; j < n; j++)  g.matrix[i][j] = 0; }
@@ -26,3 +26,43 @@ void ThemDuongMaTran(Matran &g, int u, int v, int matuyen) {
 
 struct CanhChon {  
      int u, v;   };
+
+void Prim(Matran &g) { //hàm thuận toán prim
+    int n = g.Sotinhxet;
+    bool Daduyet[Sotinh_max] = {false};  
+    Daduyet[9] = true;     
+
+    CanhChon dsCanh[Sotinh_max];
+    int soCanh = 0;
+    cout << "\nThuat toan PRIM (Bat dau tu dinh Hoa Binh):\n";
+    for (int i = 0; i < n - 1; i++) { // n đỉnh
+        int trso_min = INF;
+        int u_min = -1, v_min = -1;  // đánh dấu
+
+    for (int i = 0; i < n; i++) {
+        if (Daduyet[i]) {   // duyệt đỉnh cây khung
+            for (int j = 0; j < n; j++) {  
+            if (!Daduyet[j] && g.matrix[i][j] > 0 && g.matrix[i][j] < trso_min) { 
+                trso_min = g.matrix[i][j];    
+                u_min = i;
+                v_min = j; }  
+             } }
+        }
+     if (u_min != -1 && v_min != -1) { // nếu tìm dfduowjc đường mới
+        cout << "Chon canh " << DSTinh[u_min] << " -> " << DSTinh[v_min] << " = " << trso_min << endl;
+        Daduyet[v_min] = true;     // Kết nạp đỉnh mới vào cây
+        dsCanh[soCanh++] = {u_min, v_min};   }   // lưu vào mảng để in ra kq
+    else  break;    } 
+
+    cout << "Cây khung min: ";
+    for (int i = 0; i < soCanh; i++) {
+        cout << DSTinh[dsCanh[i].u] << " -> " << DSTinh[dsCanh[i].v]; // in ra số cạnh vừa chọn
+        if (i < soCanh - 1) {
+            cout << ", "; 
+        }
+    }
+    cout << endl;
+}
+
+
+
